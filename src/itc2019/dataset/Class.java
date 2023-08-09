ba829bd981aef1d71f142c57b6a78876b28d5a05
Sina Abdipoor <62741992+SinaAbdipoor@users.noreply.github.com>
2023-08-09 12:18:40 +0800

    Combine Event and Class
    The Event class has been incorporated into the Class class for simplicity.

--- src/itc2019/dataset/Class.java
+++ src/itc2019/dataset/Class.java
@@ -1,5 +1,6 @@
 package itc2019.dataset;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -18,6 +19,10 @@ import java.util.Arrays;
  * were described in Section Times. Each class also has a defined a set of possible rooms where it can meet (if a room
  * is needed). Each eligible room has a specified penalty to be included in the overall time penalization when selected.
  * Valid room specifications were given in Section Rooms.</p>
+ * <p>An event simply is a scheduled class that has been assigned a time, a room, and a list of students.
+ * <strong>Note that at any given time, a class can be half or not scheduled, and the time, room, and student parameters
+ * can be null. Also, if the class, as defined, does not require a room, the assigned room of its respective
+ * event is null.</strong></p>
  */
 class Class {
 
@@ -26,9 +31,13 @@ class Class {
     private final TimeAssignment[] possibleTimes;
     private final RoomAssignment[] possibleRooms;
     private final Class parent;
+    private TimeAssignment assignedTime;
+    private RoomAssignment assignedRoom;
+    private final ArrayList<Student> students;
 
     /**
-     * Constructs a Class object.
+     * Constructs an unscheduled Class object. To schedule this class, set the time, room (if required), and student
+     * parameters using the set methods.
      *
      * @param id            The id of this class.
      * @param limit         The limit (capacity) of this class.
@@ -45,6 +54,7 @@ class Class {
         this.possibleTimes = possibleTimes;
         this.possibleRooms = possibleRooms;
         this.parent = parent;
+        students = new ArrayList<>();
     }
 
     /**
@@ -92,8 +102,84 @@ class Class {
         return parent;
     }
 
+    /**
+     * Gets the class time.
+     *
+     * @return The current time period assigned to this class. <strong>Returns null if the time of this class is not
+     * currently scheduled.</strong>
+     */
+    TimeAssignment getAssignedTime() {
+        return assignedTime;
+    }
+
+    /**
+     * Assigns a time to this class.
+     *
+     * @param assignedTime The time period to schedule this class at.
+     * @throws IllegalArgumentException If the passed time assignment is invalid. <strong>To reduce the running time
+     *                                  from O(n), where n is the number of possible time assignments for this class,
+     *                                  to O(1), comment if statement in the method. However, doing so will allow
+     *                                  invalid (not included in the possible time assignments of this class) time
+     *                                  assignments.</strong>
+     */
+    void setAssignedTime(TimeAssignment assignedTime) throws IllegalArgumentException {
+        //TODO (OPTIMIZATION): For faster running time, comment the following if. However, doing so will allow invalid time assignments.
+        if (!Arrays.asList(getPossibleTimes()).contains(assignedTime))
+            throw new IllegalArgumentException("This time assignment is invalid as it is not in the list of possible time assignment of this class!");
+        //COMMENT UNTIL HERE
+        this.assignedTime = assignedTime;
+    }
+
+    /**
+     * Gets the class room.
+     *
+     * @return The current room assigned to this class. <strong>Returns null if the room of this class is not currently
+     * scheduled or if the class does not require a room.</strong>
+     */
+    RoomAssignment getAssignedRoom() {
+        return assignedRoom;
+    }
+
+    /**
+     * Assigns a room to this class.
+     *
+     * @param assignedRoom The room to schedule this class in.
+     * @throws IllegalArgumentException If this class does not require a room or if the passed room assignment is
+     *                                  invalid. <strong>To reduce the running time from O(n), where n is the number
+     *                                  of possible room assignments for this class, to O(1), comment all the if
+     *                                  statements in the method. However, doing so will allow invalid room assignments.
+     *                                  </strong>
+     */
+    void setAssignedRoom(RoomAssignment assignedRoom) throws IllegalArgumentException {
+        //TODO (OPTIMIZATION): For faster running time, comment the following if. However, doing so will allow invalid room assignments.
+        if (getPossibleRooms() == null) throw new IllegalArgumentException("This class does not require a room!");
+        if (!Arrays.asList(getPossibleRooms()).contains(assignedRoom))
+            throw new IllegalArgumentException("This room assignment is invalid as it is not in the list of possible room assignment of this class!");
+        //COMMENT UNTIL HERE
+        this.assignedRoom = assignedRoom;
+    }
+
     @Override
     public String toString() {
-        return "Class{" + "id=" + id + ", limit=" + limit + ", possibleTimes=" + Arrays.toString(possibleTimes) + ", possibleRooms=" + Arrays.toString(possibleRooms) + ", parent=" + parent + '}';
+        return "Class{" + "id=" + id + ", limit=" + limit + ", possibleTimes=" + Arrays.toString(possibleTimes) + ", possibleRooms=" + Arrays.toString(possibleRooms) + ", parent=" + parent + ", assignedTime=" + assignedTime + ", assignedRoom=" + assignedRoom + ", students=" + students + '}';
+    }
+
+    /**
+     * <p>Adds the student to this class (enrolls the students in this class).</p>
+     * <strong><p>IMPORTANT: This method does NOT check the following issues:</p>
+     * <p>1) If this class is in the list of the demanded courses of the student;</p>
+     * <p>2) If the student has been enrolled in the parent class of this class before being added here;</p>
+     * <p>3) If the student has taken one class from each subpart of a single configuration.</p>
+     * <p>Checking mechanisms of these issues should later be added here (if possible) or in other classes.</p></strong>
+     *
+     * @param student The student to add.
+     * @throws IllegalArgumentException If this class has reached its max limit.
+     */
+    void addStudent(Student student) throws IllegalArgumentException {
+        if (students.size() == getLimit()) throw new IllegalArgumentException("This class has reached max capacity!");
+        //TODO: Add checking for the student needing this class (here or in another class).
+        //TODO: Add checking for the student being enrolled in the parent class (here or in another class).
+        //TODO: Add checking for the student taking one class from each subpart of a single configuration (here or in another class).
+        students.add(student);
     }
 }
