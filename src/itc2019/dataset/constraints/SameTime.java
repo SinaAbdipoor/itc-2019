c29fcda02271d566a8e29dad3fce93ecde073052
Sina Abdipoor <62741992+SinaAbdipoor@users.noreply.github.com>
2023-08-23 10:00:55 +0800

    Create SameTime.java

---
+++ src/itc2019/dataset/constraints/SameTime.java
@@ -0,0 +1,34 @@
+package itc2019.dataset.constraints;
+
+import itc2019.dataset.Class;
+import itc2019.dataset.Event;
+
+/**
+ * <p>All classes in this constraint must be offered within the same times of day as those required by the longest class
+ * in the set, i.e., the times of day they meet must be completely overlapped by the longest class in the
+ * constraint.</p>
+ * <p>Given classes must be taught at the same time of day, regardless of their days of week or weeks. For the classes
+ * of the same length, this is the same constraint as SameStart (classes must start at the same time slot). For the
+ * classes of different lengths, the shorter class can start after the longer class but must end before or at the same
+ * time as the longer class.
+ * This means that (Ci.start ≤ Cj.start ∧ Cj.end ≤ Ci.end) ∨ (Cj.start ≤ Ci.start ∧ Ci.end ≤ Cj.end) for any two classes
+ * Ci and Cj from the constraint; Ci.end = Ci.start + Ci.length is the assigned end time slot of a class Ci.</p>
+ */
+class SameTime extends PairDistributionConstraint {
+    /**
+     * Constructs a same time distribution constraint object with the given classes.
+     *
+     * @param classes Classes that this distribution constraint applies to.
+     */
+    SameTime(Class[] classes) {
+        super(classes);
+    }
+
+    @Override
+    boolean check(Event e1, Event e2) throws NullPointerException {
+        int e1End = e1.getTimeAssignment().time().getEnd(), e2End = e2.getTimeAssignment().time().getEnd();
+        // (Ci.start ≤ Cj.start ∧ Cj.end ≤ Ci.end) ∨ (Cj.start ≤ Ci.start ∧ Ci.end ≤ Cj.end)
+        return (e1.getTimeAssignment().time().start() <= e2.getTimeAssignment().time().start() && e2End <= e1End)
+                || (e2.getTimeAssignment().time().start() <= e1.getTimeAssignment().time().start() && e1End <= e2End);
+    }
+}
