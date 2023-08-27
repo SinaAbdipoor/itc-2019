f2002f355531e1cf5cd41abe19cd118a7585c0e7
Sina Abdipoor <62741992+SinaAbdipoor@users.noreply.github.com>
2023-08-27 11:20:23 +0800

    Create NotOverlap.java

---
+++ src/itc2019/dataset/constraints/NotOverlap.java
@@ -0,0 +1,32 @@
+package itc2019.dataset.constraints;
+
+import itc2019.dataset.Class;
+import itc2019.dataset.Event;
+import utils.LogicalOperators;
+
+/**
+ * <p>No two classes in this constraint can overlap in time.</p>
+ * <p>Given classes do not overlap in time. Two classes do not overlap in time when they do not overlap in time of day,
+ * or in days of the week, or in weeks. This means that
+ * (Ci.end ≤ Cj.start) ∨ (Cj.end ≤ Ci.start) ∨ ((Ci.days and Cj.days) = 0) ∨ ((Ci.weeks and Cj.weeks) = 0) for any two
+ * classes Ci and Cj from the constraint, doing binary "and" between days and weeks of Ci and Cj.</p>
+ */
+class NotOverlap extends PairDistributionConstraint {
+    /**
+     * Constructs a not overlap distribution constraint object with the given classes.
+     *
+     * @param classes Classes that this distribution constraint applies to.
+     */
+    NotOverlap(Class[] classes) {
+        super(classes);
+    }
+
+    @Override
+    boolean check(Event e1, Event e2) throws NullPointerException {
+        // (Ci.end ≤ Cj.start) ∨ (Cj.end ≤ Ci.start) ∨ ((Ci.days and Cj.days) = 0) ∨ ((Ci.weeks and Cj.weeks) = 0)
+        return (e1.getTimeAssignment().time().getEnd() <= e2.getTimeAssignment().time().start())
+                || (e2.getTimeAssignment().time().getEnd() <= e1.getTimeAssignment().time().start())
+                || (LogicalOperators.areExclusive(e1.getTimeAssignment().time().days(), e2.getTimeAssignment().time().days()))
+                || (LogicalOperators.areExclusive(e1.getTimeAssignment().time().weeks(), e2.getTimeAssignment().time().weeks()));
+    }
+}
