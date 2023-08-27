74e96151641b922f97b59469a601d379dc044c32
Sina Abdipoor <62741992+SinaAbdipoor@users.noreply.github.com>
2023-08-27 11:34:19 +0800

    Create DifferentRoom.java

---
+++ src/itc2019/dataset/constraints/DifferentRoom.java
@@ -0,0 +1,26 @@
+package itc2019.dataset.constraints;
+
+import itc2019.dataset.Class;
+import itc2019.dataset.Event;
+
+/**
+ * <p>All classes in this constraint must be assigned to different rooms.</p>
+ * <p>Given classes should be placed in different rooms. This means that (Ci.room ≠ Cj.room) for any two classes Ci and
+ * Cj from the constraint.</p>
+ */
+class DifferentRoom extends PairDistributionConstraint {
+    /**
+     * Constructs a different room constraint object with the given classes.
+     *
+     * @param classes Classes that this distribution constraint applies to.
+     */
+    DifferentRoom(Class[] classes) {
+        super(classes);
+    }
+
+    @Override
+    boolean check(Event e1, Event e2) throws NullPointerException {
+        // (Ci.room ≠ Cj.room)
+        return e1.getRoomAssignment().room() != e2.getRoomAssignment().room();
+    }
+}
