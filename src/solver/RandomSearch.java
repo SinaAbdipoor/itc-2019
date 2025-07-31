5ac1339ccc2a5f5910e4a1ba8e9956657b51c573
Sina <62741992+SinaAbdipoor@users.noreply.github.com>
2025-07-31 17:41:56 +0800

    Create RandomSearch.java

---
+++ src/solver/RandomSearch.java
@@ -0,0 +1,32 @@
+package solver;
+
+import dataset.ProblemInstance;
+import dataset.Timetable;
+
+/**
+ * Implements the Random Search algorithm for solving the timetabling problem.
+ * This class extends the abstract Algorithm class and generates random candidate solutions.
+ */
+public class RandomSearch extends Algorithm {
+    /**
+     * Constructs a RandomSearch algorithm instance.
+     *
+     * @param instance   The problem instance to solve.
+     * @param maxSeconds The maximum allowed runtime in seconds.
+     * @param maxNfe     The maximum number of function evaluations.
+     * @throws IllegalArgumentException if any parameter is invalid.
+     */
+    public RandomSearch(ProblemInstance instance, int maxSeconds, int maxNfe) throws IllegalArgumentException {
+        super("Random Search", instance, maxSeconds, maxNfe);
+    }
+
+    @Override
+    Timetable initialize() {
+        return createRandomTimetable();
+    }
+
+    @Override
+    Timetable step() {
+        return createRandomTimetable();
+    }
+}
