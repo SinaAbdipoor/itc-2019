7ffd667f32dbbbf4e44b8a3bc400a0ed310755e7
syytt1 <143179298+mingxuan-li-official@users.noreply.github.com>
2025-12-11 14:28:02 +0800

    Running prepare

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import dataset.Timetable;
 import io.Decoder;
 import io.Encoder;
 import solver.HillClimbing;
+import solver.HillClimbingWithRS;
 import solver.Solution;
 
 public class Main {
@@ -11,38 +12,40 @@ public class Main {
             System.out.println("Step 1: Loading problem instance...");
             ProblemInstance instance = null;
             try {
-//                instance = new Encoder(args[0]).getProblemInstance();
-                instance = new Encoder("ITC2019_Dataset_Original/Original/Middle/yach-fal17.xml").getProblemInstance();
+                instance = new Encoder(args[0]).getProblemInstance();
+//                instance = new Encoder("/Users/mingxuan/OR/itc/xml/agh-fal17.xml").getProblemInstance();
                 System.out.println("Problem instance loaded successfully!");
                 instance.printStats();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             System.out.println("Step 2: Initializing algorithm...");
-//            int maxSeconds = Integer.parseInt(args[1]);
-            int maxSeconds = 10;
+            int maxSeconds = Integer.parseInt(args[1]);
+//            int maxSeconds = 1000;
 //            RandomSearch randomSearch = new RandomSearch(instance, maxSeconds, -1);
-            HillClimbing hillClimbing = new HillClimbing(instance, maxSeconds, -1);
+//            HillClimbing hillClimbing = new HillClimbing(instance, maxSeconds, -1);
+            HillClimbingWithRS hillClimbingWithRS = new HillClimbingWithRS(instance, maxSeconds, -1);
             System.out.println("Algorithm initialized successfully!");
             System.out.println("Step 3: Running algorithm...");
 //            Timetable timetable = randomSearch.run();
-            Timetable timetable = hillClimbing.run();
+//            Timetable timetable = hillClimbing.run();
+            Timetable timetable = hillClimbingWithRS.run();
 //            Solution solution = new Solution(instance.instanceName(), maxSeconds, 4, randomSearch.name, "Mingxuan", "Beijing University of Posts and Telecommunications", "China", timetable);
-            Solution solution = new Solution(instance.instanceName(), maxSeconds, 4, hillClimbing.name, "Mingxuan", "Beijing University of Posts and Telecommunications", "China", timetable);
+            Solution solution = new Solution(instance.instanceName(), maxSeconds, 4, hillClimbingWithRS.name, "Mingxuan", "Queen Mary University of London", "China", timetable);
             System.out.println("DTF: " + solution.timetable().isFeasible(instance) +
                     ", TimePenalty: " + solution.timetable().calcTimePenalty() +
                     ", RoomPenalty: " + solution.timetable().calcRoomPenalty() +
                     ", DistributionPenalty: " + solution.timetable().calcDistributionPenalty(instance));
             new Decoder(solution);
 
-//            int DTF = hillClimbing.getDTF();
-  //          int nfe = hillClimbing.getNfe();
-    //        int secondsToFeasibility = hillClimbing.getSecondsToFeasibility();
-      //      int nfeToFeasibility = hillClimbing.getNfeToFeasibility();
-        //    int[][] nfeToDTF = hillClimbing.getNfeToDTF();
+//            int DTF = hillClimbingWithRS.getDTF();
+//            int nfe = hillClimbingWithRS.getNfe();
+//            int secondsToFeasibility = hillClimbingWithRS.getSecondsToFeasibility();
+//            int nfeToFeasibility = hillClimbingWithRS.getNfeToFeasibility();
+//            int[][] nfeToDTF = hillClimbingWithRS.getNfeToDTF();
 //            int[][] nfeToDTF = randomSearch.getNfeToDTF();
-          //  InfoSaver infoSaver = new InfoSaver(instance.instanceName(), DTF, nfe, secondsToFeasibility, nfeToFeasibility, nfeToDTF);
-           // infoSaver.saveToFile();
+//            InfoSaver infoSaver = new InfoSaver(instance.instanceName(), DTF, nfe, secondsToFeasibility, nfeToFeasibility, nfeToDTF);
+//            infoSaver.saveToFile();
 //            DTFChart chart = new DTFChart(nfeToDTF, instance.instanceName());
 //            chart.plotter();
         } catch (Exception e) {
---
+++ src/solver/HillClimbingWithRS.java
@@ -0,0 +1,110 @@
+package solver;
+
+import java.util.Random;
+
+import dataset.Class;
+import dataset.Event;
+import dataset.ProblemInstance;
+import dataset.Timetable;
+
+public class HillClimbingWithRS extends LoggingAlgorithm {
+
+	/**
+	 * Constructor for algorithms with time and evaluation limits. Use -1 for
+	 * unlimited time or evaluations.
+	 *
+	 * @param instance   Problem instance to solve.
+	 * @param maxSeconds Maximum allowed seconds for the algorithm (-1 for
+	 *                   unlimited).
+	 * @param maxNfe     Maximum allowed function evaluations (-1 for unlimited).
+	 * @throws IllegalArgumentException if limits are invalid.
+	 */
+	public HillClimbingWithRS(ProblemInstance instance, int maxSeconds, int maxNfe) throws IllegalArgumentException {
+		super("Hill Climbing RS", instance, maxSeconds, maxNfe);
+	}
+
+	/**
+	 * Executes the main optimization loop for this algorithm.
+	 * <p>
+	 * This method initializes the search, then repeatedly performs search steps
+	 * until the termination condition is met (such as time or evaluation limits).
+	 * At each iteration, it prints search progress, updates feasibility and cost
+	 * information, and replaces the current best solution if a better candidate is
+	 * found. The method returns the best {@link Timetable} found. <br>
+	 * <b>Note:</b> Subclasses must ensure that the {@code initialize()} and
+	 * {@code step()} methods are implemented correctly, and that the candidate
+	 * solutions do not interfere with the current best solution.
+	 * </p>
+	 *
+	 * @return the best {@link Timetable} solution found during the search
+	 */
+	@Override
+	public void run(final Random random) {
+		long fes_until_reset = 0L;
+		Timetable best_x = null;
+		long best_f = Long.MAX_VALUE;
+		long luby_idx = 0;
+
+		while (!terminationReached()) {
+			if (--fes_until_reset <= 0L) {
+				best_x = createRandomTimetable(random);
+				best_f = this.evaluate(best_x);
+				fes_until_reset = 100L * luby(++luby_idx);
+			} else {
+				Timetable new_x = this.step(random, best_x);
+				long new_f = this.evaluate(best_x);
+				if (new_f <= best_f) {
+					best_f = new_f;
+					best_x = new_x;
+				}
+			}
+		}
+	}
+
+	/**
+	 * compute the luby restart length for the run at the given index
+	 *
+	 * @param index the index
+	 * @return the next multiplier for the number of FEs
+	 */
+	static final long luby(final long index) {
+		long twoByK, twoByKMinusOne;
+
+		for (twoByK = 1;;) {
+			twoByKMinusOne = twoByK;
+			twoByK <<= 1L;
+			if (twoByK < twoByKMinusOne) {
+				return twoByKMinusOne;
+			}
+			if (index == (twoByK - 1L)) {
+				return twoByKMinusOne;
+			}
+			if (index >= twoByK) {
+				continue;
+			}
+			return luby((index - twoByKMinusOne) + 1);
+		}
+	}
+
+	Timetable step(final Random random, Timetable use_x) {
+		Timetable candidate = use_x.deepCopy(instance);
+		Class theClass = instance.classes()[random.nextInt(instance.classes().length)];
+		Event event = candidate.getEvent(theClass);
+		if (theClass.possibleRooms() == null || random.nextInt(2) == 1) {
+			event.setTimeAssignment(theClass.possibleTimes()[random.nextInt(theClass.possibleTimes().length)]);
+		} else {
+			if (event.getAvailableRooms() != null) {
+				event.setRoomAssignment(event.getAvailableRooms()[random.nextInt(event.getAvailableRooms().length)]);
+			} else {
+				event.setRoomAssignment(theClass.possibleRooms()[random.nextInt(theClass.possibleRooms().length)]);
+			}
+		}
+		return candidate;
+	}
+
+	public static final void main(String[] args) {
+		for (int lubytest = 1; lubytest <= 10; lubytest++) {
+			System.out.println("" + lubytest + ": " + luby(lubytest));
+		}
+	}
+}
